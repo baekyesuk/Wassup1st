e65d0e03ed0ee56a88efd7e2c8cbe3be7cf6f9e2
Yesuk <bys6415@gmail.com>
2023-11-28 22:00:28 +0900

    add answer

---
+++ coffee.py
@@ -0,0 +1,13 @@
+coffee=10
+while True:
+    money = int(input("돈을 넣어주세요:"))
+    if money == 300:
+        print("커피를 줍니다.")
+        coffee=coffee-1
+    elif money>300:
+        print("거스름돈%d를 주고 커피를 줍니다." % (money-300))
+        coffee=coffee-1
+    if coffee ==0:
+        print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+        break
+        
